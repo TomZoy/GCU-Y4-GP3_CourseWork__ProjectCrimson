e3e4a41d0ae3d5cfdcc3c586e2b8d42fed436b85
TomZoy <mr.tomzoy@gmail.com>
2017-04-01 21:40:39 +0100

    additional music loaded in

--- try1_2013/Studentmain.cpp
+++ try1_2013/Studentmain.cpp
@@ -171,19 +171,16 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
 
 	// load game sounds
-	//LPCSTR gameSounds[3] = { "Audio/who10Edit.wav", "Audio/shot007.wav", "Audio/explosion2.wav" };
-	LPCSTR BGM[5] = { "Audio/BGM/wav/radio1-_01_A_Night_Of_Dizzy_Spells.wav", "Audio/BGM/wav/radio2_-01_The_Misadventure_Begins.wav", "Audio/BGM/wav/radio3_-04_Cold_as_Steel.wav", "Audio/BGM/wav/radio4_-.wav", "Audio/BGM/wav/radio5_-09_The_Day_Time_Ran_Away.wav" };
+	LPCSTR BGM[7] = { "Audio/BGM/wav/radio1-_01_A_Night_Of_Dizzy_Spells.wav", "Audio/BGM/wav/radio2_-01_The_Misadventure_Begins.wav", "Audio/BGM/wav/radio3_-04_Cold_as_Steel.wav", "Audio/BGM/wav/radio4_-.wav", "Audio/BGM/wav/radio5_-09_The_Day_Time_Ran_Away.wav", "Audio/BGM/wav/menu-music_Ziklibrenbib_Alpha_Brutal_Alpha_Brutal_Alpha_Brutal_-_06_-_EPIC_SONG.wav", "Audio/BGM/wav/Finish_line-07_Home_at_Last.wav" };
 	LPCSTR SFX[9] = { "Audio/SFX/wav/fireGun.wav", "Audio/SFX/wav/reload.wav", "Audio/SFX/wav/ricochet1.wav", "Audio/SFX/wav/ricochet2.wav", "Audio/SFX/wav/ricochet3.wav", "Audio/SFX/wav/targetHit.wav", "Audio/SFX/wav/gunEmpty.wav", "Audio/SFX/wav/bounce.wav", "Audio/SFX/wav/Mario-coin-sound.wav" };
-
-	//theSoundMgr->add("Theme", gameSounds[0]);
-	//theSoundMgr->add("Shot", gameSounds[1]);
-	//theSoundMgr->add("Explosion", gameSounds[2]);
 
 	theSoundMgr->add("BGM1", BGM[0]);
 	theSoundMgr->add("BGM2", BGM[1]);
 	theSoundMgr->add("BGM3", BGM[2]);
 	theSoundMgr->add("BGM4", BGM[3]);
 	theSoundMgr->add("BGM5", BGM[4]);
+	theSoundMgr->add("BGM6-intro", BGM[5]);
+	theSoundMgr->add("BGM7-endGame", BGM[6]);
 
 	theSoundMgr->add("fireGunSFX", SFX[0]);
 	theSoundMgr->add("reloadSFX", SFX[1]);
@@ -345,7 +342,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
 		thePlayer.update(elapsedTime);
 
 		//debug model
-		baloonMdl.renderMdl(glm::vec3(0.0f, 8.0f, 10.0f), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
+		//baloonMdl.renderMdl(glm::vec3(0.0f, 8.0f, 10.0f), 0.0f, glm::vec3(1.0f, 1.0f, 1.0f));
 
 
 
